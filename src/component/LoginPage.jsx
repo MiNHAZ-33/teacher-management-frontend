016ec143983cb8126800b993415f2fc433e05b19
MiNHAZ-33 <miminhaz33@gmail.com>
2023-06-05 21:54:07 +0600

    added login page

--- src/component/LoginPage.jsx
+++ src/component/LoginPage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col, Form, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import FormContainer from './FormContainer';
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
+    return (
+        <FormContainer>
+            <h1 className='text-center pt-4 pb-4'>Sign In</h1>
+            <Form>
+                <Form.Group >
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className='mt-2'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter password" />
+                </Form.Group>
+                <div className='text-center'>
+                <Button variant="primary" className='mt-4' type="submit">Submit</Button>
+                </div>
+            </Form>
+
+            <Row className='py-3 text-center'>
+                <Col>
+                    Doesn't have any account?{' '}
+                    <Link to={'/signup'}>
+                        Register
+                    </Link>
+                </Col>
+            </Row>
+        </FormContainer>
+    )
 }
 
 export default LoginPage
