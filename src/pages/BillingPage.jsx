8592509f9fc5d4cfd77ec506d259d12e465dfbb2
MiNHAZ-33 <miminhaz33@gmail.com>
2023-06-08 22:03:18 +0600

    fixed exam name and year issue

--- src/pages/BillingPage.jsx
+++ src/pages/BillingPage.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Table, Form, Dropdown } from 'react-bootstrap';
 
 const BillingPage = () => {
 
     const itemReducer = useSelector(state => state.billRate);
+
+    const [examNames, setExamNames] = useState("");
+
+    const [year, setYear] = useState("");
+
+    const handleExamNameChange = (index, selectedName) => {
+        setExamNames(prevNames => ({
+            ...prevNames,
+            [index]: selectedName,
+        }));
+    };
+
+    const handleYearChange = (index, selectedYear) => {
+        setYear(prevYear => ({
+            ...prevYear,
+            [index]: selectedYear,
+        }))
+    }
 
     const { items } = itemReducer;
 
@@ -31,36 +49,39 @@ const BillingPage = () => {
                             return (
                                 item.subItems.map(
                                     (subItem, subIndex) => {
+                                        const selectedIndex = `${index}-${subIndex}`;
+                                        const selectedExamName = examNames[selectedIndex] || '';
+                                        const selectedYear = year[selectedIndex] || '';
                                         return (
                                             <tr key={subIndex}>
                                                 <td>{subItem.name}</td>
                                                 <td>
                                                     <Dropdown>
                                                         <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                                                            Choose Exam Name
+                                                            {selectedExamName ? selectedExamName : 'Choose Exam Name'}
                                                         </Dropdown.Toggle>
-                                                        <Dropdown.Menu>
-                                                            <Dropdown.Item>B.Sc Engineering</Dropdown.Item>
-                                                            <Dropdown.Item>Bachelor of Science</Dropdown.Item>
-                                                            <Dropdown.Item>Bachelor of Arts</Dropdown.Item>
-                                                            <Dropdown.Item>BBA</Dropdown.Item>
-                                                            <Dropdown.Item>MBA</Dropdown.Item>
-                                                            <Dropdown.Item>MA</Dropdown.Item>
+                                                        <Dropdown.Menu >
+                                                            <Dropdown.Item onClick={() => handleExamNameChange(selectedIndex, "B.Sc Engineering")}>B.Sc Engineering</Dropdown.Item>
+                                                            <Dropdown.Item onClick={() => handleExamNameChange(selectedIndex, "Bachelor of Science")}> Bachelor of Science</Dropdown.Item>
+                                                            <Dropdown.Item onClick={() => handleExamNameChange(selectedIndex, "Bachelor of Arts")}>Bachelor of Arts</Dropdown.Item>
+                                                            <Dropdown.Item onClick={() => handleExamNameChange(selectedIndex, "BBA")}>BBA</Dropdown.Item>
+                                                            <Dropdown.Item onClick={() => handleExamNameChange(selectedIndex, "MBA")}>MBA</Dropdown.Item>
+                                                            <Dropdown.Item onClick={() => handleExamNameChange(selectedIndex, "MA")}>MA</Dropdown.Item>
                                                         </Dropdown.Menu>
                                                     </Dropdown>
                                                 </td>
                                                 <td>
                                                     <Dropdown>
                                                         <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                                                            Choose Year
+                                                            {selectedYear ? selectedYear : 'Choose year'}
                                                         </Dropdown.Toggle>
                                                         <Dropdown.Menu>
-                                                            <Dropdown.Item>2023</Dropdown.Item>
-                                                            <Dropdown.Item>2022</Dropdown.Item>
-                                                            <Dropdown.Item>2021</Dropdown.Item>
-                                                            <Dropdown.Item>2020</Dropdown.Item>
-                                                            <Dropdown.Item>2019</Dropdown.Item>
-                                                            <Dropdown.Item>2018</Dropdown.Item>
+                                                            <Dropdown.Item onClick={() => handleYearChange(selectedIndex, "2023")} >2023</Dropdown.Item>
+                                                            <Dropdown.Item onClick={() => handleYearChange(selectedIndex, "2022")}>2022</Dropdown.Item>
+                                                            <Dropdown.Item onClick={() => handleYearChange(selectedIndex, "2021")}>2021</Dropdown.Item>
+                                                            <Dropdown.Item onClick={() => handleYearChange(selectedIndex, "2020")}>2020</Dropdown.Item>
+                                                            <Dropdown.Item onClick={() => handleYearChange(selectedIndex, "2019")}>2019</Dropdown.Item>
+                                                            <Dropdown.Item onClick={() => handleYearChange(selectedIndex, "2018")}>2018</Dropdown.Item>
                                                         </Dropdown.Menu>
                                                     </Dropdown>
                                                 </td>
@@ -80,7 +101,7 @@ const BillingPage = () => {
                                                     <Form.Control type="number" placeholder='Paper Type' />
                                                 </td>
                                                 <td>
-                                                    <h6 className='text-white text-center'>{ subItem.price  }</h6>
+                                                    <h6 className='text-white text-center'>{subItem.price}</h6>
                                                 </td>
                                             </tr>
                                         )
@@ -104,9 +125,7 @@ const BillingPage = () => {
                         <td>
                             <h6 className='text-white text-center'>50000</h6>
                         </td>
-                        
                     </tr>
-
                 </tbody>
             </Table>
             <h2>Hi</h2>
@@ -120,3 +139,4 @@ const BillingPage = () => {
 }
 
 export default BillingPage;
+
